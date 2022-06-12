1e4e30d7f078ff662d4051cf8da6703b779b4b1f
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-06-12 14:12:58 +0700

    Upload Angular CV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { BlogComponent } from './components/client/blog/blog.component';
+import { HomePageComponent } from './components/client/home-page/home-page.component';
+import { ClientLayoutComponent } from './layouts/client-layout/client-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: ClientLayoutComponent,
+  children: [
+          { path: '', component: HomePageComponent },
+          { path: 'blog', component: BlogComponent },
+        ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientLayoutComponent } from './layouts/client-layout/client-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { FooterComponent } from './components/client/footer/footer.component';
+import { HeaderComponent } from './components/client/header/header.component';
+import { HomePageComponent } from './components/client/home-page/home-page.component';
+import { BannerComponent } from './components/client/banner/banner.component';
+import { BlogComponent } from './components/client/blog/blog.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientLayoutComponent,
+    AdminLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomePageComponent,
+    BannerComponent,
+    BlogComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
